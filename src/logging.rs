383fce40e8794b30026240d0ce91a533edb32d49
Denis Tsai <denistsai@aetheras.io>
2021-04-01 19:21:36 +0800

    updating the logging to not overwrite with code level pattern.  Include log level and colors. Yay

--- src/logging.rs
+++ src/logging.rs
@@ -1,17 +1,30 @@
 pub fn init_logger(pattern: &str) {
     use ansi_term::Colour;
     use chrono::Utc;
+    use log::Level;
     use std::io::Write;
 
     let mut builder = env_logger::Builder::new();
+    builder.parse_filters(pattern);
     if let Ok(lvl) = std::env::var("RUST_LOG") {
         builder.parse_filters(&lvl);
     }
-    builder.parse_filters(pattern);
 
     builder.format(move |buf, record| {
         let time_now = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        let output = format!("{} {}", Colour::Blue.bold().paint(time_now), record.args(),);
+        let level = match record.level() {
+            Level::Error => Colour::Red.bold().paint("ERR"),
+            Level::Warn => Colour::Yellow.bold().paint("WRN"),
+            Level::Info => Colour::Blue.bold().paint("INF"),
+            Level::Debug => Colour::Green.bold().paint("DBG"),
+            Level::Trace => Colour::Cyan.bold().paint("TRC"),
+        };
+        let output = format!(
+            "[{}] {} {}",
+            level,
+            Colour::Blue.bold().paint(time_now),
+            record.args(),
+        );
         writeln!(buf, "{}", output)
     });
 
