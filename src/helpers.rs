fe72d631994ed6cbf94b7d10a0376caa26dcd911
Denis Tsai <denistsai@aetheras.io>
2022-07-20 18:26:28 +0800

    add comments to helpers

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,6 @@
 /// Backoff calculator based off google's algorithm
 /// #NOTE, jitter is randomized in the range of 0-1000 ms
+/// `max_backoff_ms` defaults to 32000ms if none is given which is the min suggested by google
 pub fn exponential_backoff_ms(count: u64, max_backoff_ms: Option<u64>) -> u64 {
     use rand::Rng;
     let mut rng = rand::thread_rng();
