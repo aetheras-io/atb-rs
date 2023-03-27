84aad06cf354399b0b940bc89c918add1bb1889a
Denis Tsai <denistsai@aetheras.io>
2023-03-27 18:16:37 +0800

    refactor serde-ext into modules for simpler upstream usage using #[serde(with=...)]

--- ext/serde/src/lib.rs
+++ ext/serde/src/lib.rs
@@ -1,54 +1,66 @@
 use serde::{Deserialize, Deserializer, Serializer};
 
-pub fn ser_i64_as_string<S>(x: &i64, se: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    se.serialize_str(&x.to_string())
-}
+pub mod i64_as_string {
+    use super::*;
 
-pub fn de_string_as_i64<'de, D>(de: D) -> Result<i64, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let id = String::deserialize(de)?;
-    id.parse().map_err(serde::de::Error::custom)
-}
+    pub fn serialize<S>(x: &i64, se: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        se.serialize_str(&x.to_string())
+    }
 
-pub fn ser_empty_string_as_none<S>(x: &Option<String>, se: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    match x {
-        None => se.serialize_none(),
-        Some(s) if s.is_empty() => se.serialize_none(),
-        Some(s) => se.serialize_some(s),
+    pub fn deserialize<'de, D>(de: D) -> Result<i64, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let id = String::deserialize(de)?;
+        id.parse().map_err(serde::de::Error::custom)
     }
 }
 
-pub fn de_empty_as_none<'de, D, T>(de: D) -> Result<Option<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let opt = Option::<String>::deserialize(de)?;
-    match opt {
-        None => Ok(None),
-        Some(s) if s.is_empty() => Ok(None),
-        _ => Ok(opt),
+pub mod empty_string_none {
+    use super::*;
+
+    pub fn serialize<S>(x: &Option<String>, se: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match x {
+            None => se.serialize_none(),
+            Some(s) if s.is_empty() => se.serialize_none(),
+            Some(s) => se.serialize_some(s),
+        }
+    }
+
+    pub fn deserialize<'de, D, T>(de: D) -> Result<Option<String>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let opt = Option::<String>::deserialize(de)?;
+        match opt {
+            None => Ok(None),
+            Some(s) if s.is_empty() => Ok(None),
+            _ => Ok(opt),
+        }
     }
 }
 
-pub fn ser_bytes_as_base64<T, S>(bytes: &T, se: S) -> Result<S::Ok, S::Error>
-where
-    T: AsRef<[u8]>,
-    S: serde::Serializer,
-{
-    se.serialize_str(&base64::encode(bytes.as_ref()))
-}
+pub mod bytes_as_base64 {
+    use super::*;
 
-pub fn de_base64_as_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    String::deserialize(de).and_then(|s| base64::decode(&s).map_err(serde::de::Error::custom))
+    pub fn serialize<T, S>(bytes: &T, se: S) -> Result<S::Ok, S::Error>
+    where
+        T: AsRef<[u8]>,
+        S: serde::Serializer,
+    {
+        se.serialize_str(&base64::encode(bytes.as_ref()))
+    }
+
+    pub fn deserialize<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        String::deserialize(de).and_then(|s| base64::decode(&s).map_err(serde::de::Error::custom))
+    }
 }
